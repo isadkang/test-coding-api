399fcb7bf2f3e9ae4d1e63ae3ec609b0fedce2b0
isadkang <akungamef543@gmail.com>
2023-07-05 19:01:38 +0700

    first commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Latihan Coding
+## Interaction with API 
+
+```javascript
+    endPoint = 'http://(https://pokeapi.co/api/v2/berry)'
+```
+
+### Tugas Sederhana
+
+1. Buatlah list dropdown dengan data diatas beserta fitur input search didalamnya (done)
+2. Filter data tersebut secara ASC (done)
+3. Munculkan output dari hasil pilihan data (done)
---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css"
+import Select from "react-select"
+import { useEffect, useState } from "react"
+
+function App() {
+  const [datas, setDatas] = useState([]) // State untuk menampung data dari API
+  const [userSelect, setUserSelect] = useState("") // State untuk menampung pilihan dari pengguna yang diambil dari API
+  const [isShow, setIsShow] = useState(false) // State untuk tombol menampilkan pilihan / select pengguna
+
+  const getBerries = async () => {
+    const berries = await fetch("https://pokeapi.co/api/v2/berry") // Mengambil data berry dari API
+    const value = await berries.json() // Mengonversi data berry menjadi objek JSON
+    let result = value.results.map(data => {
+      return {
+        label: data.name, // Menyimpan nama berry sebagai label
+        value: data.name, // Menyimpan nama berry sebagai value
+      }
+    })
+    setDatas(result.sort((a,b) => a.label.localeCompare(b.label))) // Mengupdate state 'datas' dengan hasil pengolahan data berry dari API
+    // console.log(datas)
+  } // Fungsi untuk mengambil data dari API dan mengurutkannya secara ASCENDING (berurutan dari A - Z) berdasarkan label berry
+
+  useEffect(() => {
+    getBerries() // Memanggil fungsi getBerries saat komponen dirender pertama kali
+  },[]) 
+
+  const handleSubmit = () => {
+    setIsShow(state => !state) // Mengubah nilai state 'isShow' menjadi kebalikan dari nilai sebelumnya
+  }
+
+  const handleChange = (value) => {
+    setUserSelect(value) // Mengubah nilai state 'userSelect' dengan nilai yang dipilih oleh pengguna
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={() => handleSubmit()} disabled={!userSelect}>{isShow ? "Sembunyikan Nilai" : "Tampilkan Nilai"}</button> 
+        {/* Tombol untuk menampilkan/menyembunyikan nilai */}
+        <br/>
+        <br/>
+        <Select options={datas} onChange={(e) => handleChange(e.value)}></Select>
+        {/* Komponen Select untuk memilih nilai dari 'datas' dan memanggil handleChange saat nilai berubah */}
+        <br/>
+        <h1>{isShow ? userSelect : ""}</h1>
+        {/* Menampilkan nilai 'userSelect' jika 'isShow' bernilai true */}
+      </div>
+    </>
+  );
+}
+
+export default App
